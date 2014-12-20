80b5b2c91fa3aee3ade8d118c628b14eba6f0978
Niall Hugh <niall@rocstudio.co.uk>
2014-12-20 15:40:32 +0000

    date update

--- holding-page/js/scripts.js
+++ holding-page/js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "20 december 2014 12:00:00", /** Enter new date here **/
+		date: "20 febuary 2015 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
